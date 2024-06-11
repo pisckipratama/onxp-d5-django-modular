2a4b9df91da5229e3714c8e0fc0579b8629bb870
Piscki Pratama <pisckipratama@gmail.com>
2024-06-11 10:40:46 +0700

    add param and request path

--- src/app_landing/urls.py
+++ src/app_landing/urls.py
@@ -5,5 +5,7 @@ from .views import my_view, about_view
 
 urlpatterns = [
     path('', my_view, name="home"),
-    path('about/', about_view, name="about")
+    path('about/', about_view, name="about"),
+    path('about/<int:id>', about_view, name='about'),
+    path('about/<int:id>/<int:year>', about_view, name='about')
 ]
--- src/app_landing/views.py
+++ src/app_landing/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 
+def agregasi(num):
+    return num + 1
+
 def my_view(request):
     return render(request, 'app_landing/index.html', { 'name': 'Landing' })
 
-def about_view(request):
-    return render(request, 'app_landing/about.html', {'name': 'About'})
+def about_view(request, id: int = 0, year: int = 2024):
+    
+
+    context = {
+        "nama": f"Piscki {id}",
+        "agregasi": f"{agregasi(id)}",
+        "year": year,
+        "limit": request.GET.get("limit", 0)
+    }
+    return render(request, 'app_landing/about.html', context=context)
